d11886962b9a95869729a7263b89e37dc7d2eefa
Alejandro Gabriel Corzo <alejandro.corzo@techforb.com>
2023-05-24 21:18:13 -0300

    encuentro 32

---
+++ guia7-introduccionAJava/src/com/java/guia7/Main.java
@@ -0,0 +1,119 @@
+package com.java.guia7;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+        String nombre = "Ale";
+        int edad = 29;
+//        ejercicio4(nombre, edad);
+//        ejercicio5();
+//        ejercicio6();
+//        ejercicio7();
+//        ejercicio9();
+//        ejercicio10();
+//        ejercicio11();
+//        ejercicio12();
+
+    }
+
+    public static void ejercicio13() {
+        Scanner read = new Scanner(System.in);
+
+    }
+
+    public static void ejercicio12() {
+        Scanner read = new Scanner(System.in);
+        int n1 = read.nextInt();
+        int n2 = read.nextInt();
+        if (esMultiple(n1, n2)) System.out.println("Son múltiplos.");
+        else System.out.println("No son múltiplos.");
+    }
+
+    public static boolean esMultiple(int n1, int n2) {
+        return n2 % n1 == 0;
+    }
+
+    public static void ejercicio11() {
+        Scanner read = new Scanner(System.in);
+        String entry = read.nextLine();
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < entry.length(); i++) {
+            switch (entry.charAt(i)) {
+                case 'a' -> builder.append('@');
+                case 'e' -> builder.append('#');
+                case 'i' -> builder.append('$');
+                case 'o' -> builder.append('%');
+                case 'u' -> builder.append('*');
+                default -> builder.append(entry.charAt(i));
+            }
+        }
+        System.out.println(builder);
+    }
+
+    public static void ejercicio10() {
+        Scanner read = new Scanner(System.in);
+        int n;
+        for (int i = 0; i < 4; i++) {
+            n = read.nextInt();
+            for (int j = 0; j < n; j++) {
+                if (j == 0) System.out.print(n);
+                if (j == n - 1) {
+                    System.out.println("*");
+                } else {
+                    System.out.print("*");
+                }
+            }
+        }
+    }
+
+    public static void ejercicio9() {
+        Scanner read = new Scanner(System.in);
+        int entry, q = 0;
+        while (q < 5) {
+            entry = read.nextInt();
+            if (entry == 0) {
+                System.out.println("Se ha encontrado un 0.");
+                break;
+            }
+            ++q;
+        }
+    }
+
+    public static void ejercicio7() {
+        Scanner read = new Scanner(System.in);
+        int numMotor = read.nextInt();
+        switch (numMotor) {
+            case 1 -> System.out.println("Bomba de Agua.");
+            case 2 -> System.out.println("Bomba de Gasolina");
+            case 3 -> System.out.println("Bomba de Hormigón.");
+            case 4 -> System.out.println("Bomba de Pasta Alimenticia.");
+            default -> System.out.println("No existe un valor válido para tipo de bomba");
+        }
+    }
+
+    public static void ejercicio6() {
+        Scanner read = new Scanner(System.in);
+        int num1 = read.nextInt();
+        int num2 = read.nextInt();
+        if (num1 > 25) System.out.println("num1 mayor que 25");
+        if (num2 > 25) System.out.println("num2 mayor que 25");
+    }
+
+    public static void ejercicio5() {
+        Scanner read = new Scanner(System.in);
+        System.out.println("Ingresa tu edad");
+        int edad = read.nextInt();
+        read.nextLine();
+        System.out.println("Ingresa tu nombre");
+        String nombre = read.nextLine();
+        System.out.println("Soy " + nombre + " y tengo " + edad);
+    }
+
+    public static void ejercicio4(String nombre, int edad) {
+//        String x, nom = "Ale;
+        System.out.println("Mi nombre es " + nombre + ", y tengo " + edad + " años.");
+    }
+
+}
