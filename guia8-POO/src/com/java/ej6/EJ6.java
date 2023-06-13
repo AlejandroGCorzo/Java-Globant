d4983b507545e68457baa028aced14b58e25ea3f
AlejandroGCorzo <alejandro.g.corzo@gmail.com>
2023-06-13 19:26:15 -0300

    probando2

--- guia8-POO/src/com/java/ej6/EJ6.java
+++ guia8-POO/src/com/java/ej6/EJ6.java
@@ -3,6 +3,6 @@ package com.java.ej6;
 public class EJ6 {
     public static void main(String[] args) {
         Cafetera nespresso = new Cafetera(10, 2.5);
-        String probando = "probando";
+        String probando = "probando2";
     }
 }
