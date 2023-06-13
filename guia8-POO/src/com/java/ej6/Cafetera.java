100345e55e44f1772b91ab89c60fbd45e451c8a6
Alejandro Gabriel Corzo <alejandro.corzo@techforb.com>
2023-06-13 19:21:00 -0300

    guia 8 POO - clase 42 - Ej6

---
+++ guia8-POO/src/com/java/ej6/Cafetera.java
@@ -0,0 +1,62 @@
+package com.java.ej6;
+
+public class Cafetera {
+    private double capacidadMaxima;
+    private double cantidadActual;
+
+    public Cafetera() {
+    }
+
+    public Cafetera(double capacidadMaxima, double cantidadActual) {
+        this.capacidadMaxima = capacidadMaxima;
+        this.cantidadActual = cantidadActual;
+    }
+
+    private double getCapacidadMaxima() {
+        return capacidadMaxima;
+    }
+
+    private void setCapacidadMaxima(double capacidadMaxima) {
+        this.capacidadMaxima = (double) Math.round(capacidadMaxima * 100) / 100;
+    }
+
+    private double getCantidadActual() {
+        return cantidadActual;
+    }
+
+    private void setCantidadActual(double cantidadActual) {
+        this.cantidadActual = (double) Math.round(cantidadActual * 100) / 100;
+    }
+
+    public void llenarCafetera() {
+        setCantidadActual(getCapacidadMaxima());
+        System.out.println("Cafetera llena kpo.");
+    }
+
+    public void servirTaza(int taza) {
+        if (taza > getCantidadActual()) {
+            System.out.println("No se llenó la taza. Contiene " + getCantidadActual() / taza + "% de la taza.");
+            setCantidadActual(0);
+        } else {
+            System.out.println("Taza llena");
+            setCantidadActual(getCantidadActual() - taza);
+        }
+    }
+
+    public void vaciarCafetera() {
+        setCantidadActual(0);
+        System.out.println("Se ha vaciado la cafetera.");
+    }
+
+    public void agregarCafe(int cantidad) {
+        if (getCantidadActual() + cantidad <= getCapacidadMaxima()) {
+            setCantidadActual(getCantidadActual() + cantidad);
+            System.out.println("Se ha añadido " + cantidad + " a la cafetera.");
+        } else {
+            setCantidadActual(getCapacidadMaxima());
+            System.out.println("Se ha añadido sólo " + (getCapacidadMaxima() - getCantidadActual()) + " debido a que sobrepasaba el límite.");
+        }
+
+    }
+
+}
